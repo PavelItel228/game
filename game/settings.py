0b585000f1d1db49e93a3eba5ebe886e3c58434c
PavelItel228 <44823566+PavelItel228@users.noreply.github.com>
2019-07-30 13:35:37 +0300

    Update settings.py

--- game/settings.py
+++ game/settings.py
@@ -12,9 +12,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 import django_heroku
-import django
 SECRET_KEY = 'hw6tz+ot*e8#2ix75^emkk%ta@id5x+grk95nk%6rlt7^i7@ss'
-django.setup()
 
 
 
@@ -147,5 +145,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
-django_heroku.settings(locals())
 
