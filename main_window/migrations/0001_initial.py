fdcffcc58dcc60f38853c15dc41ac4d587d5801d
PavelItel228 <44823566+PavelItel228@users.noreply.github.com>
2019-07-30 14:19:10 +0300

    Create 0001_initial.py

---
+++ main_window/migrations/0001_initial.py
@@ -0,0 +1,17 @@
+class Migration(migrations.Migration):
+
+    initial = True
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='OnlineUser',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('user', models.OneToOneField(on_delete=django.db.models.deletion.CASCADE, related_name='online_user', to=settings.AUTH_USER_MODEL)),
+            ],
+        ),
+    ]
